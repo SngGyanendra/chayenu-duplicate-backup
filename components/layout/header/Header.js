03ef7aaf20bc3c044a2d284a6b4b6c0170f00020
Gyanendra Singh <gyanendra@gorinsystems.com>
2023-02-03 13:24:12 +0530

    CHSUB-325: Add header to website

---
+++ components/layout/header/Header.js
@@ -0,0 +1,67 @@
+import Image from 'next/image';
+import Styles from './header.module.scss';
+import { useRef } from 'react';
+
+export function Header() {
+  const handleMenu = () => {
+    const element = document.getElementsByClassName('menu')[0];
+    element.classList.toggle('menuClose');
+  };
+  const handleClickedOutside = (e) => {
+    if (!menu.current.contains(e.target)) {
+      handleMenu();
+    }
+  };
+  const menu = useRef();
+
+  return (
+    <header className={Styles.header}>
+      <div className={Styles.mobileHeader}>
+        <div>
+          <Image
+            src="/hamburger.svg"
+            alt="menu"
+            height={30}
+            width={30}
+            loading="lazy"
+            onClick={handleMenu}
+          />
+        </div>
+        <div>
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            height={50}
+            width={80}
+            loading="lazy"
+          />
+        </div>
+      </div>
+      <div
+        className={`${Styles.translucentLayer} menuClose menu`}
+        onClick={(e) => {
+          handleClickedOutside(e);
+        }}
+      >
+        <div className={Styles.container} ref={menu}>
+          <div>
+            <Image
+              src="/logo.svg"
+              alt="logo"
+              height={50}
+              width={80}
+              loading="lazy"
+            />
+          </div>
+          <div>
+            <ul>
+              <li>SUBSCRIBE</li>
+              <li>EXPLORE</li>
+            </ul>
+          </div>
+          <div>LOGIN</div>
+        </div>
+      </div>
+    </header>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, 'styles'),
+      path.join(__dirname, 'components'),
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
