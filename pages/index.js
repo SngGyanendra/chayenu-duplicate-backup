d698a7e4301bfe45f563ec3dfdc075ca82364f83
Gyanendra Singh <gyanendra@gorinsystems.com>
2023-02-28 21:29:08 +0530

    CHSUB-329 : Fix outlines for hover

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Styles from '/styles/home.module.scss';
 export default function Home() {
   return (
     <div className={Styles.home}>
-      <Image src="/design.png" alt="" height={500} width={500} />
     </div>
   );
 }
