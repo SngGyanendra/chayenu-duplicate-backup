32e1ee5d6da67b2710bc950e6155673e1bd493ed
Gyanendra Singh <gyanendra@gorinsystems.com>
2023-05-22 19:42:15 +0530

    CHUB-449:Fix white logo

--- components/common/NextHead.js
+++ components/common/NextHead.js
@@ -6,8 +6,8 @@ export function NextHead({ title, description }) {
       <title>{title}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta property="og:title" content={title}></meta>
-      {/* <meta property="og:description" content={description}></meta> */}
-      <meta property="og:image" content="/logo.svg"></meta>
+      <meta property="og:description" content={description}></meta>
+      <meta property="og:image" content="/logodarker.svg"></meta>
       {description && <meta name="description" content={description} />}
     </Head>
   );
