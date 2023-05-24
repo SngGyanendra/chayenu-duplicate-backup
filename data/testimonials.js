a12697988c543509160c008219c8e61fc695c8ae
Ravindra <ravindra@gorinsystems.com>
2023-05-24 09:26:47 +0530

    CHSUB-453: More SEO improvements

--- data/testimonials.js
+++ data/testimonials.js
@@ -2,12 +2,12 @@ export const testimonials1 = [
     {
         statement: 'Chayenu makes it easier for me to learn Torah on a daily basis, especially when I am traveling on my concert tours. There are so many blessings that come from it. It\'s enhanced my life in so many ways.',
         designation: 'Esther Freeman',
-        imageUrl: '/images/homepage/Testimonials/Esther1.png'
+        imageUrl: '/images/homepage/Testimonials/Esther1.webp'
     },
     {
         statement: 'I usually make time for learning on Shabbat. I really enjoy the Ein Yaakov. Certain things I think I would never have studied if it wasn\'t in Chayenu, like Rambam. The Chassidic story is interesting to me. And they added a letter from the Rebbe.',
         designation: 'Dr. Bob Shorr, Ventura, CA',
-        imageUrl: '/images/homepage/Testimonials/Bob1.png'
+        imageUrl: '/images/homepage/Testimonials/Bob1.webp'
     },
 ]
 
@@ -15,10 +15,10 @@ export const testimonials2 = [
     {
         statement: 'I love my Chayenu app! The app, on the front page of my phone, helps me feel my connection to Hashem as part of my morning routine. Besides the daily portion, my favorite reads are the “Name of the Parsha” and the Chassidic Story.”',
         designation: 'Alice Harron, San Francisco, CA',
-        imageUrl: '/images/homepage/Testimonials/Alice1.png'
+        imageUrl: '/images/homepage/Testimonials/Alice1.webp'
     },
     {   statement: 'I am a huge fan. I read the app every day. It has been life transforming and gives me the critical daily study I need for the day in an easily accessible way.',
         designation: 'Arnie Herz, Port Washington, NY',
-        imageUrl: '/images/homepage/Testimonials/Arnie1.png'
+        imageUrl: '/images/homepage/Testimonials/Arnie1.webp'
     },
 ]
