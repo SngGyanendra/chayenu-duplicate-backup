a3fda9a5e243c1bffd60ba9cb7f7f45127694b11
Ravindra <ravindra@gorinsystems.com>
2023-05-30 18:46:53 +0530

    CHSUB-460: Redirection added

--- next.config.js
+++ next.config.js
@@ -12,6 +12,30 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/product-category/military',
+        destination: '/subscribe?is_military_only=true',
+        permanent: false,
+      },
+      {
+        source: '/product-tag/student',
+        destination: '/subscribe?student_only=true',
+        permanent: false,
+      },
+      {
+        source: '/product/:slug',
+        destination: '/subscribe',
+        permanent: false,
+      },
+      {
+        source: '/product-category/:slug',
+        destination: '/subscribe',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
