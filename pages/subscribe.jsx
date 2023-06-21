a94ca989d907c188e33869d541290b77b2165d57
Gyanendra Singh <gyanendra@gorinsystems.com>
2023-06-21 22:36:11 +0530

    fix

--- pages/subscribe.jsx
+++ pages/subscribe.jsx
@@ -127,7 +127,7 @@ export default function Subscribe({ query }) {
                   );
                 })
               : allProducts.map((product, index) => {
-                  if (product?.plans?.length === 0) return null;
+                  // if (product?.plans?.length === 0) return null;
                   return (
                     <div
                       key={product.id}
