0633697063affb09d5a4e0d7d8a89b65bf8c9536
Zeeshan Qaisar <zeeshan@gorinsystems.com>
2024-02-12 19:34:01 +0500

    CHSUB-653:updated tab title

--- pages/forgot-password.jsx
+++ pages/forgot-password.jsx
@@ -65,7 +65,7 @@ export default function ForgotPassword() {
 
   return (
     <div className={Styles.forgotPasswordContainer}>
-      <NextHead title="Chayenu | Forgot Password" />
+      <NextHead title="Chayenu | Reset Password" />
       {forgotPasswordStage === 'OTP' ? (
         <>
           <h1 className={Styles.emailText}>Reset Password</h1>
